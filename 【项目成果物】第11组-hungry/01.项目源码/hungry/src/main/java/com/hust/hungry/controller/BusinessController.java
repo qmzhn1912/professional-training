ddf2ee8f9c32411b9cdfc03293a2baa4b2ae5658
HUSTERandENDER <15272984770@163.com>
2024-06-27 16:35:08 +0800

    """"""

--- 【项目成果物】第11组-hungry/01.项目源码/hungry/src/main/java/com/hust/hungry/controller/BusinessController.java
+++ 【项目成果物】第11组-hungry/01.项目源码/hungry/src/main/java/com/hust/hungry/controller/BusinessController.java
@@ -74,4 +74,18 @@ public class BusinessController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedBusiness);
     }
 
+    @PostMapping("/update/{businessId}")
+     public JsonResult update(@RequestParam(value = "businessName") String businessName,
+                              @RequestParam(value = "starPrice") Float starPrice,
+                              @RequestParam(value = "businessAddress") String businessAddress,
+                              @RequestParam(value = "deliveryPrice") Float deliveryPrice,
+                              @PathVariable("businessId")Integer businessId) {
+        Business business = businessMapper.selectById(businessId);
+        business.setBusinessName(businessName);
+        business.setStarPrice(starPrice);
+        business.setBusinessAddress(businessAddress);
+        business.setDeliveryPrice(deliveryPrice);
+        businessMapper.updateById(business);
+        return new JsonResult(business);
+    }
 }
